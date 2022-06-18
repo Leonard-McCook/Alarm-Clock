fed5f8325b9fbad4a079974b2be888f0eefd3cf9
Leonard-McCook <leonard.mccookcarr@outlook.com>
2022-06-18 01:14:49 -0400

    Add for loop for hours setting

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const selectMenu = document.querySelectorAll("select");
 
 for (let i = 12; i > 0; i--) {
-    console.log(i);
+    i = i < 10 ? "0" + i : i;
+    let option = `<option value="${i}">${i}</option>`;
+    selectMenu[0] .firstElementChild.insertAdjacentHTML("afterend", option);
+
+
 }
 
